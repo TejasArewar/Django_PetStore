06282109b3b46748cf5aedbd26f77ed296661e78
TejasArewar <104343599+TejasArewar@users.noreply.github.com>
2024-11-30 15:31:22 +0530

    Update settings.py

--- PetStore/settings.py
+++ PetStore/settings.py
@@ -149,4 +149,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'tejas.arewar.ta@gmail.com'
-EMAIL_HOST_PASSWORD = 'nzfg qyyz xywz uczm'
+EMAIL_HOST_PASSWORD = ''
